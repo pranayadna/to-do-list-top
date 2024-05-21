c5768213a4cc1e2ab8ac048d1590751bd3b65f0a
pranayadna <pranayadnaap96@gmail.com>
2024-05-21 16:41:30 +0700

    Add function for create project and to-do list, and to-do item

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 function createTodoList() {
-    const todos = [];
+    const todoList = [];
 
-    const getTodos = () => todos;
+    const getTodoList = () => todoList;
 
-    const getSpecificTodos = (index) => todos[index];
+    const getSpecificTodo = (index) => todos[index];
     
     const addTodoItem = () => {
         const todo = createTodoItem();
-        todos.push(todo);
+        todoList.push(todo);
+        return todo;
     };
 
-    return { getTodos, getSpecificTodos, addTodoItem };
+    return { getTodoList, getSpecificTodo, addTodoItem };
 }
 
 function createTodoItem() {
-    let title = '';
-    let description = '';
+    let title = 'Title Template';
+    let description = 'This is description template...';
     let dueDate = '30/05/2024';
     let priority = 'Low';
 
@@ -61,6 +62,43 @@ function createTodoItem() {
     };
 }
 
-const todos = createTodoList();
-const addTodo1 = todos.addTodoItem();
-const todo1 = todos.getSpecificTodos(0);
+function createProjectList() {
+    const projects = [];
+
+    const addTodoList = () => {
+        const todoList = createTodoList();
+        projects.push(todoList);
+        return todoList;
+    }
+
+    const getProjects = () => projects;
+
+    return { addTodoList, getProjects };
+}
+
+// Add to-do list to project list
+const projects1 = createProjectList();
+const projects2 = createProjectList();
+
+// Add to-do item to to-do list
+const todoList1 = projects1.addTodoList();
+const todoList2 = projects1.addTodoList();
+
+const addTodo1 = todoList1.addTodoItem();
+
+console.log({
+    title: addTodo1.getTitle(),
+    description: addTodo1.getDescription(),
+    dueDate: addTodo1.getDueDate(),
+    priority: addTodo1.getPriority(),
+});
+
+console.log({
+    todo1_item: todoList1.getTodoList(),
+    todo2_item: todoList2.getTodoList(),
+});
+
+console.log({
+    project1_list: projects1.getProjects(),
+    project2_list: projects2.getProjects(),
+});
